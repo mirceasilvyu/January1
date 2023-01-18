5cd8ba8997423c69733f5a5e90dcaca1b18d9776
Silvyu <mirceasilvyu@gmail.com>
2023-01-18 18:22:58 +0200

    Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        int i =2;
+        int j =0;
+        boolean value=(i!=0) || (j!=0);
+        System.out.println("The value is :"+value);
+
+
+
+    }
+}
